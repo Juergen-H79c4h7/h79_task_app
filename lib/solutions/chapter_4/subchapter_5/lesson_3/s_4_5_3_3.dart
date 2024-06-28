a340725be1e4e2ae787d8405ee9d1f976ab63ee2
Hoovey@gmx.de <hoovey@gmx.de>
2024-06-28 16:05:54 +0200

    1st BLA

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,34 @@ class S4533 extends StatelessWidget {
   const S4533({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: Container(
+        width: 150,
+        height: 150,
+        color: Colors.blue,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'App',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 89),
+            Text(
+              'Akademie',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
